47ca41f24320d59498487977e6ca52a9a59e13b7
Michelle <nyamburamichelle400@gmail.com>
2024-03-21 19:51:44 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1=31
+let num2=2
+let multiply=num1*num2
 
-// Write your code here
+
+let x=122
+let y=10
+let random =x%y
+
+
+let num3= 32
+let num4=8
+let mod= num3/num4
+
+
+let max =Math.max(9,18,9,8,15,20)
+
+
